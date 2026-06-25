95f1349ce59f5ebe99b77e697b8f7563ec558eef
faisalkhalildev <sfaisalshah2024@gmail.com>
2026-06-25 20:17:19 +0500

    Make login Api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,36 @@
-let users = [
-    {id:1 , email:"shah@gmail.com", name:"shahfaisal"},
-    {id:2 , email:"abs@gmail.com", name:"faisal"},
-    {id:3 , email:"abc@gmail.com", name:"faisalshah"},
-];
+const bcrypt = require("bcrypt")
+const users = [];
 
-exports.addUser = (req , res) => {
-    
+exports.register = async (req , res) => {
+    const {username , email, password} = req.body;
+    if(!username || !email || !password){
+        return res
+          .status(400)
+          .send({isSucess : false , message:"All fields are required"});
+    }
+    try {
+        const  foundUser = users.find((item) => 
+            item.username === username && item.email === email
+         );
+         if(foundUser){
+            return res
+             .status(409)
+             .send({isSucess: false , message:"username and email already exist"})
+         }
+
+         const salt = await bcrypt.genSalt(10);
+         const hashPassword = await bcrypt.hash(password, salt);
+         console.log(hashPassword)
+
+         const user = {username , email , password:hashPassword}
+         users.push(user)
+
+         res.send({isSucess: true , message: "User sucessfully Registered!"})
+    } catch(error) {
+        console.error("error")
+    }
+}
+
+exports.getUsers = async (req, res) => {
+    res.send({isSucess: true , data:users})
 }
